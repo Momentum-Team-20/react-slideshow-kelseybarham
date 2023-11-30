1297a256647dfe36afe64b4f106881f9a64ca023
Kelsey Barham <kelseylbarham@gmail.com>
2023-11-30 15:42:10 -0500

    completed set up, web page is connected

---
+++ slideshow/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import filmData from './data/film-data.json'
+
+
+function App() {
+  return (
+    <>
+    <h1>Studio Ghibli slideshow</h1>
+    </>
+  )
+}
+
+export default App
